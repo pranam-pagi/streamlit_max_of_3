f45a1eb327438a9faffdfd9712cf3fda6108eec3
Pranam Pagi <101988406+pranam-pagi@users.noreply.github.com>
2023-08-31 21:43:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ def main():
     st.title("Find the Largest Number")
     st.write("Enter three numbers below to find the largest among them:")
 
-    a = st.number_input("Enter the first number:")
-    b = st.number_input("Enter the second number:")
-    c = st.number_input("Enter the third number:")
+    a = st.number_input("Enter the first number:", step=1.00)
+    b = st.number_input("Enter the second number:", step=1.00)
+    c = st.number_input("Enter the third number:", step=1.00)
 
     if st.button("Find Largest"):
         largest = max(a, b, c)
